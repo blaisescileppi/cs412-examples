9512ba892c0d64c6009a4e80d8931b360dc93ebf
Blaise Scileppi <blaises@bu.edu>
2026-03-07 22:10:45 -0500

    fixed assignment 6, profile views and following fixed

--- cs412/urls.py
+++ cs412/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static    ## add for static files
 from django.conf import settings
 
 
+
 urlpatterns = [ 
     path('admin/', admin.site.urls),
     path('hw/', include("hw.urls")),
