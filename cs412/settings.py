2eb79d80fe0d2fc87cd86c8473491db76064651e
Blaise Scileppi <blaises@bu.edu>
2026-03-20 17:22:08 -0400

    fixing css

--- cs412/settings.py
+++ cs412/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/' # note: no leading slash!
+STATIC_URL = 'static/' # note: no leading slash!
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
