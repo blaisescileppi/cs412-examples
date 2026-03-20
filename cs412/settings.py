d51d2a46d9f66fa9f4451ea335e503655ee4afc3
Blaise Scileppi <blaises@bu.edu>
2026-03-20 17:27:16 -0400

    fixing css

--- cs412/settings.py
+++ cs412/settings.py
@@ -123,8 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = 'static/' # note: no leading slash!
+
+STATIC_URL = '/blaises/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/' # note: no leading slash!
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
